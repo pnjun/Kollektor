90eb61f160e551fcd90df917d4d6bf1e603e6b16
Fabiano Lever <leverfab@gmail.com>
2022-01-15 16:09:30 +0100

    Fixed tag removal order and textbox focus

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivymd.uix.toolbar import MDToolbar
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 from kivy.properties import NumericProperty
+from kivy.uix.textinput import TextInput
 
 from pony import orm
 
@@ -20,9 +21,9 @@ class Tag(MDBoxLayout):
     # Takes a db tag handle and populates the tag gui with the appropriate info
     def __init__(self, tag, *args, **kwargs):
         super(Tag, self).__init__(*args, **kwargs)
-        self.dbTag = tag
+        self.db_tag = tag
         self.color = tag.color
-        self.ids.tagLabel.text = tag.name
+        self.ids.tag_label.text = tag.name
 
 
 class Entry(MDCard):
@@ -31,40 +32,76 @@ class Entry(MDCard):
     # Takes a db entry handle and populates the gui with the appropriate info
     def __init__(self, entry, *args, **kwargs):
         super(Entry, self).__init__(*args, **kwargs)
-        self.dbEntry = entry
+        self.db_entry = entry
         self.ids.description.text = entry.description
         for tag in entry.tags:
             gui_tag = Tag(tag)
             self.ids.tagList.add_widget(gui_tag)
 
 
+# TODO: create decorator that sets system_cursor in a context manager
 class EntryList(MDBoxLayout):
-    @orm.db_session
-    def update(self):
+
+    def search_entries(self, tag_list):
         app = MDApp.get_running_app()
 
-        for entry in app.kollektion.Entry.select():
+        if tag_list:
+            return app.kollektion.Entry.select(lambda e: tag_list[0] in e.tags)
+        return []
+
+    @orm.db_session
+    def update(self):
+        Window.set_system_cursor('wait')
+
+        app = MDApp.get_running_app()
+        tag_list = [t.db_tag for t in app.tag_bar.children]
+
+        for entry in self.search_entries(tag_list):
             gui_entry = Entry(entry)
             self.add_widget(gui_entry)
+        Window.set_system_cursor('arrow')
 
 
 class TagBar(MDBoxLayout):
     def on_children(self, instance, val):
         app = MDApp.get_running_app()
-        app.root.ids.entryList.update()
+        app.root.ids.entry_list.update()
 
 
-class HeadBar(MDToolbar):
+class TagSearch(TextInput):
+
+    def keyboard_on_key_down(self, keyboard, keycode, text, modifiers):
+        """ Overrides a backspace and enter key presses for custom behaviour """
+        _, key_name = keycode
+        if key_name == 'backspace' and self.text == '':
+            app = MDApp.get_running_app()
+            try:
+                app.tag_bar.remove_widget(app.tag_bar.children[0])
+            except IndexError:
+                pass
+        elif key_name == 'enter':
+            # Catch the on_enter event and block it so that the textInput doesn't lose focus
+            # since, the standard on_text_validate removes focus from the textInput.
+            self.on_enter()
+        else:
+            super().keyboard_on_key_down(keyboard, keycode, text, modifiers)
+
     @orm.db_session
-    def validate_tag(self, textbox):
+    def on_text(*args):
+        """ TODO: give hints for autocomplete"""
+        pass
+
+    @orm.db_session
+    def on_enter(self):
+        """ Load appropriate tags from DB and add it to the tag bar"""
         app = MDApp.get_running_app()
 
-        # tags = app.kollektion.Tag().select(lambda t: t.name == textbox.text)
-        query_tag = orm.select(t for t in app.kollektion.Tag if t.name == textbox.text).first()
+        query_tag = app.kollektion.Tag.select(lambda t: t.name.lower() == self.text.lower()).first()
+
         if query_tag is not None:
+            self.text = ''
             gui_tag = Tag(query_tag)
-            textbox.text = ''
-            app.tagBar.add_widget(gui_tag)
+            app.tag_bar.add_widget(gui_tag)
 
 
 class Kollektor(MDApp):
@@ -77,7 +114,8 @@ class Kollektor(MDApp):
     # Populate entry list, to be changed
     def on_start(self):
         self.kollektion = Kollektion('db.sqlite')
-        self.root.ids.entryList.update()
+        self.tag_bar = self.root.ids.filter_bar.ids.tag_bar
+        self.root.ids.entry_list.update()
 
 
 # Entry point
